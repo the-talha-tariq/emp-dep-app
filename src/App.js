bff5ce6838e788a1f5b25fabb81fd67e89710a95
Talha Tariq <tariqtalha936@gmail.com>
2023-08-02 14:52:50 +0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import {Home} from './Home';
+import {Department} from './Department';
+import {Employee} from './Employee';
+import { BrowserRouter as Router, Route, Link,NavLink,Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="App container">
+      <h3 className="d-flex justify-content-center m-3">
+        Rect JS FrontEnd
+      </h3>
+
+      <nav className='navbar navbar-expand-sm bg-light navbar-dark'>
+        <ul className='navbar-nav'>
+          <li className='nav-item- m-1'>
+            <NavLink className="btn btn-light btn-outline-primary" to="/home">Home</NavLink>
+          </li>
+        </ul>
+        <ul className='navbar-nav'>
+          <li className='nav-item- m-1'>
+            <NavLink className="btn btn-light btn-outline-primary" to="/department">Department</NavLink>
+          </li>
+        </ul>
+        <ul className='navbar-nav'>
+          <li className='nav-item- m-1'>
+            <NavLink className="btn btn-light btn-outline-primary" to="/employee">Employee</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route exact path="/home" element={<Home/>} />
+        <Route exact path="/department" element={<Department/>} />
+        <Route exact path="/employee" element={<Employee/>} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
---
+++ src/Variable.js
@@ -0,0 +1,4 @@
+export const variables={
+    API_URL:"http://localhost:44997/api/",
+    PHOTO_URL:"http://localhost:44997/Photos/"
+}
